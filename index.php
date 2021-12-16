c7c093fe0bfe67ae39399f5cf160b0f745e70478
dokkalemon <dottori.roberto@gmail.com>
2021-12-16 16:20:57 +0100

    stamp string length

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 $text = 'Ciao Ragazzi come sta andando il corso Boolean Careers?';
 
 #Stampiamo il contenuto della variabile su schermo
-echo $text
+echo $text;
+
+echo '<br>';
+
+#stampiamo la lunghezza dell stringa
+echo strlen($text);
 
 ?>
