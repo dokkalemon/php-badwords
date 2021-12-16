efbaa5d204f91c4f2f99848a25b2348bfbb2bf4c
dokkalemon <dottori.roberto@gmail.com>
2021-12-16 16:27:50 +0100

    modify a word with asterisks and mofify the lenght

--- index.php
+++ index.php
@@ -11,4 +11,12 @@ echo '<br>';
 #stampiamo la lunghezza dell stringa
 echo strlen($text);
 
+echo '<br>';
+
+$textModified = str_replace($_GET["word"], '***', $text);
+
+#cambiamo una parola decisa dall'utente dall query String in 3 asterischi
+echo $textModified;
+echo strlen($textModified);
+
 ?>
